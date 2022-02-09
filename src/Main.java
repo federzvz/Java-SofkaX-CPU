a7db2aa05a98ae8f1990ac742ece6a826fc4e1c9
federzvz <85709865+federzvz@users.noreply.github.com>
2022-02-09 19:51:25 -0300

    Funcionalidades nuevas
    Funcionalidades:
    - Se implementó INV
    - Se implementó ADD

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        String[] subrutina = {"MOV 5,R00", "MOV 10,R01", "JZ 7", "ADD R02,R01", "DEC R00", "JMP 3", "MOV R02,R42"};
+            String[] subrutina = {"MOV 5,R00", "MOV 10,R01", "JZ 7", "ADD R02,R01", "DEC R00", "JMP 3", "MOV R02,R42"};
         procesarInstrucciones(subrutina);
     }
 
-    public static void procesarInstrucciones(String[] instruccines){
+    public static void procesarInstrucciones(String[] instrucciones){
         int[] array = new int[43];
         int i=1;
-        while(i<=instruccines.length){
-            switch (identificarComando(instruccines,i-1)){
+        while(i<=instrucciones.length){
+            switch (identificarComando(instrucciones,i-1)){
                 case 1://Metodo MOV
                     //System.out.println("Instruccion:"+i+" es de tipo MOV");
                     //obtenerSegundoArgumentoDeTresElementos(instruccines[0]);
-                    metodoMOV(instrucciones[i-1], array)
+                    metodoMOV(instrucciones[i-1], array);
                     i++;
                     break;
                 case 2://Metodo ADD
                     //System.out.println("Instruccion:"+i+" es de tipo ADD");
+                    metodoADD(instrucciones[i-1], array);
                     i++;
                     break;
-                case 3://Metodo DEC
+                case 3://Metodo DEC FINALIZADO: FEDERICO
                     //System.out.println("Instruccion:"+i+" es de tipo DEC");
-                    System.out.println(ProcesoStringToIntDadoDosElementos(instruccines[i-1]));
+                    //System.out.println(ProcesoStringToIntDadoDosElementos(instrucciones[i-1]));
+                    metodoDEC(ProcesoStringToIntDadoDosElementos(instrucciones[i-1]),array);
                     i++;
                     break;
                 case 4://Metodo INC
                    // System.out.println("Instruccion:"+i+" es de tipo INC");
                     i++;
                     break;
-                case 5://Metodo INV
+                case 5://Metodo INV FINALIZADO: FEDERICO
                     //System.out.println("Instruccion:"+i+" es de tipo INV");
+                    metodoINV(ProcesoStringToIntDadoDosElementos(instrucciones[i-1]),array);
                     i++;
                     break;
                 case 6://Metodo JMP
                     //System.out.println("Instruccion:"+i+" es de tipo JMP");
                     //System.out.println(obtenerUnicoArgumentoDadoDosElementos(instruccines[i-1]));
-                    i++;
+                    i = metodoJMP(instrucciones[i-1]);
                     break;
                 case 7://Metodo JZ
                     //System.out.println("Instruccion:"+i+" es de tipo JZ");
@@ -50,6 +53,7 @@ public class Main {
                     break;
             }
         }
+        System.out.print(array[42]);
     }
 
     public static int identificarComando(String[] subrutina, int indiceDeInstrucciones){
@@ -106,7 +110,7 @@ public class Main {
         return Integer.parseInt(instruccionCompleta.substring(indexInicial+1, indexFinal));
     }
 
-    //Obtiene segundo argumento de JZ,JMP
+    //Obtiene segundo argumento de JZ,JMP -->> Este metodo está modificado(obtenerUnicoElemento)
     public static int obtenerUnicoArgumentoDadoDosElementos(String instruccionCompleta){
         if(instruccionCompleta.contains("JZ") || instruccionCompleta.contains("JMP")) {
             int indexInicial = instruccionCompleta.indexOf(' ');
@@ -142,6 +146,10 @@ public class Main {
 
         return item.substring(++indexInicial);
     }
+    public static int obtenerUnicoElemento (String item) {
+            int index = item.indexOf(' ');
+            return Integer.parseInt(item.substring(++index));
+    }
 
     public static int posicionArray (String argumento) {
         String posicionSinR = argumento.substring(1);
@@ -163,6 +171,35 @@ public class Main {
             array[posicionArg2] = argDecimal;
         }
     }
+    public static void metodoADD(String item, int[] array) {
+            //Obtiene los argumentos
+            String arg1 = obtenerPrimerElemento(item);
+            String arg2 = obtenerSegundoElemento(item);
 
+            //Los argumentos son posicion del array
+            int posicionArg1 = posicionArray(arg1);
 
+            int posicionArg2 = posicionArray(arg2);
+
+            array[posicionArg1] += array[posicionArg2];
+    }
+
+    public static void metodoDEC(int indexRegistro, int[] array){
+        array[indexRegistro]--;
+    }
+
+    public static void metodoINV(int indexRegistro, int[] array){
+        int valorRegistro=array[indexRegistro];
+        //Convertir Int to Binario
+        String binario = Integer.toBinaryString(valorRegistro);
+        //Invertir Binario
+        binario.replace('0', '2').replace('1', '0').replace('2', '1');
+        //Convertir binario to Int
+        int valorRegistroBinarioInvertido= Integer.parseInt(binario, 2);
+        array[indexRegistro]=valorRegistroBinarioInvertido;
+    }
+
+    public static int metodoJMP(String item) {
+           return obtenerUnicoElemento(item);
+    }
 }
